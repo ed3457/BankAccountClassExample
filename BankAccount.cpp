bd12d29a4e754d99a9b8272c35746adaee563c9f
Kamel R <kamel.r@wayne.edu>
2024-05-16 18:23:17 -0400

    added the code for the set/get functions

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,1 +1,30 @@
 #include "BankAccount.h"
+
+void BankAccount::setClientName(string cn)
+{
+	clientName = cn;
+
+}
+
+string BankAccount::getClientName()
+{
+	return clientName; 
+
+}
+
+void BankAccount::setBranchName(string bn)
+{
+	branchName = bn;
+
+}
+
+string BankAccount::getBranchName()
+{
+	return branchName;
+
+}
+
+float BankAccount::getBalance()
+{
+	return balance;
+}
